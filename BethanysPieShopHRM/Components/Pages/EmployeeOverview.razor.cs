7337a7168ee538406f4ab5dc4bf6e314e0fe73b8
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2025-09-14 12:46:16 -0500

    Employee overview and details

---
+++ BethanysPieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.Components.Pages;
+
+public partial class EmployeeOverview
+{
+    public List<Employee> Employees { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        Employees = MockDataService.Employees!;
+    }
+}
